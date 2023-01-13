3267bd5e92c625ce664cb91bbeb6eacd545fbc7d
Alexey Votentsev <votencevaleksejj@gmail.com>
2023-01-13 14:11:16 +0500

    main.html desktop version added

--- dist/js/swipers.js
+++ dist/js/swipers.js
@@ -2,16 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Пересчет rem в px для swiper
     const rem = function (rem) {
-        if ($(window).width() > 768) {
-            return 0.005208335 * $(window).width() * rem;
+        if (window.innerWidth > 768) {
+            return 0.005208335 * window.innerWidth * rem;
         } else {
             // где 375 это ширина моб версии макета
-            return (100 / 375) * (0.1 * $(window).width()) * rem;
+            return (100 / 375) * (0.1 * window.innerWidth) * rem;
         }
     }
 
-    const getTwoDigitNumber = function (number) {
-        if (number < 10) return String('0' + number)
+    const getThreeDigitNumber = function (number) {
+        if (number < 10) return String('00' + number)
+        else if (number > 10 && number < 100) return String('0' + number)
         else return String(number)
     }
 
@@ -22,34 +23,73 @@ document.addEventListener('DOMContentLoaded', function () {
         allowTouchMove: false,
         slidesPerView: 'auto',
 
-        // navigation: {
-        //     nextEl: '.intro__swiper-next',
-        //     prevEl: '.intro__swiper-prev',
-        // },
+        navigation: {
+            nextEl: '.main-hero__swiper-next',
+            prevEl: '.main-hero__swiper-prev',
+        },
 
-        // pagination: {
-        //     el: '.intro__swiper-pagination',
-        // },
-        // breakpoints: {
-        //     320: {
-        //         pagination: {
-        //             type: 'custom',
-        //             renderCustom: function (swiper, current, total) {
-        //                 return `<span class="current">${getTwoDigitNumber(current)}</span>`;
-        //             }
-        //         },
-        //     },
-        //     769: {
-        //         pagination: {
-        //             renderBullet: function (index, className) {
-        //                 if (index < 10) {
-        //                     return '<span class="' + className + '">' + '0' + (index + 1) + '</span>';
-        //                 } else {
-        //                     return '<span class="' + className + '">' + (index + 1) + '</span>';
-        //                 }
-        //             }
-        //         },
-        //     }
-        // },
+        pagination: {
+            el: '.main-hero__swiper-pagination',
+            renderBullet: function (index, className) {
+                return `<span class="${className}">${getThreeDigitNumber(index + 1)}</span>`;
+            }
+        },
+    });
+
+    const mainServicesThumbsSwiper = new Swiper('.main-services__thumbs', {
+        spaceBetween: 0,
+        slidesPerView: 3,
+        // freeMode: true,
+        // watchSlidesProgress: true,
+        allowTouchMove: false,
+    });
+
+    const mainServicesSwiper = new Swiper('.main-services__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        spaceBetween: rem(3),
+        // allowTouchMove: false,
+        slidesPerView: 'auto',
+
+
+        thumbs: {
+            swiper: mainServicesThumbsSwiper,
+        },
+
+        navigation: {
+            nextEl: '.main-services__swiper-next',
+            prevEl: '.main-services__swiper-prev',
+        },
+
+        pagination: {
+            el: '.main-services__swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
+            }
+        },
+    });
+
+    const mainAboutSwiper = new Swiper('.main-about__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        spaceBetween: rem(3),
+        // allowTouchMove: false,
+        slidesPerView: 'auto',
+
+        navigation: {
+            nextEl: '.main-about__swiper-next',
+            prevEl: '.main-about__swiper-prev',
+        },
+
+        pagination: {
+            el: '.main-about__swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
+            }
+        },
     });
 })
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    
+    // Пересчет rem в px
+    const rem = function (rem) {
+        if (window.innerWidth > 768) {
+            return 0.005208335 * window.innerWidth * rem;
+        } else {
+            // где 375 это ширина моб версии макета
+            return (100 / 375) * (0.1 * window.innerWidth) * rem;
+        }
+    }
+
+
+    // карта на главной странице
+    if (document.querySelector('#main-contacts-map')) {
+        ymaps.ready(init);
+        function init() {
+            const myMap = new ymaps.Map("main-contacts-map", {
+                center: [55.816708, 37.611281],
+                zoom: 16
+            });
+
+            const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
+                {
+                    iconLayout: 'default#image',
+                    // Своё изображение иконки метки.
+                    iconImageHref: '../images/svg/map-point.svg',
+                    // Размеры метки.
+                    iconImageSize: [rem(12.8), rem(12.8)],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [rem(-6.4), rem(-6.4)]
+                })
+            myMap.behaviors.disable('scrollZoom')
+            myMap.geoObjects.add(homaPlacemark);
+        }
+
+
+    }
 })
