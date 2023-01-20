a64180c4b84028eba81b4d323764966d7c790d6a
Alexey Votentsev <votencevaleksejj@gmail.com>
2023-01-20 13:36:40 +0500

    main.html finished, services.html finished

--- dist/js/swipers.js
+++ dist/js/swipers.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
         else return String(number)
     }
 
+    // ГЛАВНАЯ
+
     const mainHeroSwiper = new Swiper('.main-hero__swiper', {
         direction: 'horizontal',
         loop: true,
@@ -86,6 +88,129 @@ document.addEventListener('DOMContentLoaded', function () {
 
         pagination: {
             el: '.main-about__swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
+            }
+        },
+    });
+
+    const mainPartnersSwiper = new Swiper('.main-partners__swiper', {
+        direction: 'horizontal',
+        loop: false,
+        speed: 500,
+        // allowTouchMove: false,
+        slidesPerView: 1,
+        slidesPerColumn: 2,
+
+        navigation: {
+            nextEl: '.main-partners__swiper-next',
+            prevEl: '.main-partners__swiper-prev',
+        },
+    });
+
+
+    // УСЛУГИ
+
+    const servicesCleaningThumbsSwiper = new Swiper('.services-cleaning__thumbs', {
+        spaceBetween: 0,
+        slidesPerView: 3,
+        // freeMode: true,
+        // watchSlidesProgress: true,
+        allowTouchMove: false,
+    });
+
+    const servicesCleaningSwiper = new Swiper('.services-cleaning__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        spaceBetween: rem(3),
+        // allowTouchMove: false,
+        slidesPerView: 'auto',
+
+
+        thumbs: {
+            swiper: servicesCleaningThumbsSwiper,
+        },
+
+        navigation: {
+            nextEl: '.services-cleaning__swiper-next',
+            prevEl: '.services-cleaning__swiper-prev',
+        },
+
+        pagination: {
+            el: '.services-cleaning__swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
+            }
+        },
+    });
+
+    const servicesFeedingThumbsSwiper = new Swiper('.services-feeding__thumbs', {
+        spaceBetween: 0,
+        slidesPerView: 2,
+        // freeMode: true,
+        // watchSlidesProgress: true,
+        allowTouchMove: false,
+    });
+
+    const servicesFeadingSwiper = new Swiper('.services-feeding__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        spaceBetween: rem(3),
+        // allowTouchMove: false,
+        slidesPerView: 'auto',
+
+
+        thumbs: {
+            swiper: servicesFeedingThumbsSwiper,
+        },
+
+        navigation: {
+            nextEl: '.services-feeding__swiper-next',
+            prevEl: '.services-feeding__swiper-prev',
+        },
+
+        pagination: {
+            el: '.services-feeding__swiper-pagination',
+            type: 'custom',
+            renderCustom: function (swiper, current, total) {
+                return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
+            }
+        },
+    });
+
+
+    const servicesExploitationThumbsSwiper = new Swiper('.services-exploitation__thumbs', {
+        spaceBetween: 0,
+        slidesPerView: 4,
+        // freeMode: true,
+        // watchSlidesProgress: true,
+        allowTouchMove: false,
+    });
+
+    const servicesExploitationSwiper = new Swiper('.services-exploitation__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        spaceBetween: rem(3),
+        // allowTouchMove: false,
+        slidesPerView: 'auto',
+
+
+        thumbs: {
+            swiper: servicesExploitationThumbsSwiper,
+        },
+
+        navigation: {
+            nextEl: '.services-exploitation__swiper-next',
+            prevEl: '.services-exploitation__swiper-prev',
+        },
+
+        pagination: {
+            el: '.services-exploitation__swiper-pagination',
             type: 'custom',
             renderCustom: function (swiper, current, total) {
                 return `<span class="current">${getThreeDigitNumber(current)}</span><span class="total">${getThreeDigitNumber(total)}</span>`;
--- src/js/main.js
+++ src/js/main.js
@@ -14,27 +14,47 @@ document.addEventListener('DOMContentLoaded', function () {
     // карта на главной странице
     if (document.querySelector('#main-contacts-map')) {
         ymaps.ready(init);
+
         function init() {
-            const myMap = new ymaps.Map("main-contacts-map", {
-                center: [55.816708, 37.611281],
-                zoom: 16
-            });
+            if (window.innerWidth > 768) {
+                const myMap = new ymaps.Map("main-contacts-map", {
+                    center: [55.816804, 37.617042],
+                    zoom: 16
+                });
 
-            const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
-                {
-                    iconLayout: 'default#image',
-                    // Своё изображение иконки метки.
-                    iconImageHref: '../images/svg/map-point.svg',
-                    // Размеры метки.
-                    iconImageSize: [rem(12.8), rem(12.8)],
-                    // Смещение левого верхнего угла иконки относительно
-                    // её "ножки" (точки привязки).
-                    iconImageOffset: [rem(-6.4), rem(-6.4)]
-                })
-            myMap.behaviors.disable('scrollZoom')
-            myMap.geoObjects.add(homaPlacemark);
+                const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
+                    {
+                        iconLayout: 'default#image',
+                        // Своё изображение иконки метки.
+                        iconImageHref: '../images/svg/map-point.svg',
+                        // Размеры метки.
+                        iconImageSize: [rem(12.8), rem(12.8)],
+                        // Смещение левого верхнего угла иконки относительно
+                        // её "ножки" (точки привязки).
+                        iconImageOffset: [rem(-6.4), rem(-6.4)]
+                    })
+                myMap.behaviors.disable('scrollZoom')
+                myMap.geoObjects.add(homaPlacemark);
+            } else {
+                const myMap = new ymaps.Map("main-contacts-map", {
+                    center: [55.816804, 37.617042],
+                    zoom: 16
+                });
+
+                const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
+                    {
+                        iconLayout: 'default#image',
+                        // Своё изображение иконки метки.
+                        iconImageHref: '../images/svg/map-point.svg',
+                        // Размеры метки.
+                        iconImageSize: [rem(6), rem(6)],
+                        // Смещение левого верхнего угла иконки относительно
+                        // её "ножки" (точки привязки).
+                        iconImageOffset: [rem(-3), rem(-3)]
+                    })
+                myMap.behaviors.disable('scrollZoom')
+                myMap.geoObjects.add(homaPlacemark);
+            }
         }
-
-
     }
 })
