228bc82d57600ad592fe28595ddd6cc96904c04c
Alexey Votentsev <votencevaleksejj@gmail.com>
2023-01-21 04:04:14 +0500

    contacts.html finished + mobile menu added

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    // моб меню
+
+    const burger = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
+    const body = document.body;
+    burger.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        body.classList.toggle('lock');
+        burger.classList.toggle('active');
+    })
+
 
     // карта на главной странице
     if (document.querySelector('#main-contacts-map')) {
@@ -37,6 +48,54 @@ document.addEventListener('DOMContentLoaded', function () {
                 myMap.geoObjects.add(homaPlacemark);
             } else {
                 const myMap = new ymaps.Map("main-contacts-map", {
+                    center: [55.816804, 37.617042],
+                    zoom: 16
+                });
+
+                const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
+                    {
+                        iconLayout: 'default#image',
+                        // Своё изображение иконки метки.
+                        iconImageHref: '../images/svg/map-point.svg',
+                        // Размеры метки.
+                        iconImageSize: [rem(6), rem(6)],
+                        // Смещение левого верхнего угла иконки относительно
+                        // её "ножки" (точки привязки).
+                        iconImageOffset: [rem(-3), rem(-3)]
+                    })
+                myMap.behaviors.disable('scrollZoom')
+                myMap.geoObjects.add(homaPlacemark);
+            }
+        }
+    }
+
+    // карта на странице Контакты
+
+    if (document.querySelector('#contacts-main-map')) {
+        ymaps.ready(init);
+
+        function init() {
+            if (window.innerWidth > 768) {
+                const myMap = new ymaps.Map("contacts-main-map", {
+                    center: [55.815731, 37.617258],
+                    zoom: 16
+                });
+
+                const homaPlacemark = new ymaps.Placemark([55.815787, 37.619486], {},
+                    {
+                        iconLayout: 'default#image',
+                        // Своё изображение иконки метки.
+                        iconImageHref: '../images/svg/map-point.svg',
+                        // Размеры метки.
+                        iconImageSize: [rem(10), rem(10)],
+                        // Смещение левого верхнего угла иконки относительно
+                        // её "ножки" (точки привязки).
+                        iconImageOffset: [rem(-3), rem(-3)]
+                    })
+                myMap.behaviors.disable('scrollZoom')
+                myMap.geoObjects.add(homaPlacemark);
+            } else {
+                const myMap = new ymaps.Map("contacts-main-map", {
                     center: [55.816804, 37.617042],
                     zoom: 16
                 });
